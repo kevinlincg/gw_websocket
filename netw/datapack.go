6fa7c33a6599376d69247c012fc841c964deaf97
KevinLin <kaiwei.lin@gmail.com>
2022-01-05 18:53:09 +0800

    Changes DataPack binary from LittleEndian to BigEndian

--- netw/datapack.go
+++ netw/datapack.go
@@ -15,10 +15,10 @@ func NewDataPack() iface.Packet {
 
 func (dp *DataPack) Pack(msg iface.Message) ([]byte, error) {
 	dataBuff := bytes.NewBuffer([]byte{})
-	if err := binary.Write(dataBuff, binary.LittleEndian, msg.GetMsgID()); err != nil {
+	if err := binary.Write(dataBuff, binary.BigEndian, msg.GetMsgID()); err != nil {
 		return nil, err
 	}
-	if err := binary.Write(dataBuff, binary.LittleEndian, msg.GetData()); err != nil {
+	if err := binary.Write(dataBuff, binary.BigEndian, msg.GetData()); err != nil {
 		return nil, err
 	}
 	return dataBuff.Bytes(), nil
@@ -27,10 +27,10 @@ func (dp *DataPack) Pack(msg iface.Message) ([]byte, error) {
 func (dp *DataPack) Unpack(binaryData []byte) (iface.Message, error) {
 	dataBuff := bytes.NewReader(binaryData)
 	msg := &Message{}
-	if err := binary.Read(dataBuff, binary.LittleEndian, &msg.ID); err != nil {
+	if err := binary.Read(dataBuff, binary.BigEndian, &msg.ID); err != nil {
 		return nil, err
 	}
-	if err := binary.Read(dataBuff, binary.LittleEndian, &msg.Data); err != nil {
+	if err := binary.Read(dataBuff, binary.BigEndian, &msg.Data); err != nil {
 		return nil, err
 	}
 	return msg, nil
