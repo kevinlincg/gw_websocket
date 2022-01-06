7a42b7db5a326164c3eea17d778df44adb4cddf9
KevinLin <kaiwei.lin@gmail.com>
2022-01-06 15:58:33 +0800

    feat: change default datapack from binary pack to json pack

--- gwnet/datapack.go
+++ gwnet/datapack.go
@@ -1,8 +1,7 @@
 package gwnet
 
 import (
-	"bytes"
-	"encoding/binary"
+	"encoding/json"
 
 	"github.com/kevinlincg/gw_websocket/gwiface"
 )
@@ -14,24 +13,12 @@ func NewDataPack() gwiface.Packet {
 }
 
 func (dp *DataPack) Pack(msg gwiface.Message) ([]byte, error) {
-	dataBuff := bytes.NewBuffer([]byte{})
-	if err := binary.Write(dataBuff, binary.BigEndian, msg.GetMsgID()); err != nil {
-		return nil, err
-	}
-	if err := binary.Write(dataBuff, binary.BigEndian, msg.GetData()); err != nil {
-		return nil, err
-	}
-	return dataBuff.Bytes(), nil
+	msgN := Message{msg.GetMsgID(), msg.GetData()}
+	return json.Marshal(&msgN)
 }
 
 func (dp *DataPack) Unpack(binaryData []byte) (gwiface.Message, error) {
-	dataBuff := bytes.NewReader(binaryData)
-	msg := &Message{}
-	if err := binary.Read(dataBuff, binary.BigEndian, &msg.ID); err != nil {
-		return nil, err
-	}
-	if err := binary.Read(dataBuff, binary.BigEndian, &msg.Data); err != nil {
-		return nil, err
-	}
-	return msg, nil
+	var msg Message
+	json.Unmarshal(binaryData, &msg)
+	return &msg, nil
 }
